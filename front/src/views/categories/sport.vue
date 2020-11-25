9e60384cb03dbf593f59be48dd85e71df8f23e9e
NicolasRoche <nicolas8roche@gmail.com>
2020-11-25 18:38:41 +0100

    [#10] Add new files for new routes coming

---
+++ front/src/views/categories/sport.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <el-container direction="vertical">
+      <el-header height="">
+        <!-- Header content -->
+        <h1>Header {{name}} content</h1>
+      </el-header>
+      <el-container direction="horizontal">
+        <el-aside width="200px">
+          <!-- Aside content -->
+        <p>Aside content</p>
+        </el-aside>
+        <el-container direction="vertical">
+          <el-main height="">
+            <!-- Main content -->
+          <p>Main content</p>
+          </el-main>
+          <el-footer height="">
+            <!-- Footer content -->
+          </el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+// import { UserModule } from '@/store/modules/user'
+
+@Component({
+  name: 'Sport'
+})
+export default class extends Vue {
+  get name() {
+    return 'Sport'
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ front/src/views/topquestions/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <el-container direction="vertical">
+      <el-header height="">
+        <!-- Header content -->
+        <h1>Header {{name}} content</h1>
+      </el-header>
+      <el-container direction="horizontal">
+        <el-aside width="200px">
+          <!-- Aside content -->
+        <p>Aside content</p>
+        </el-aside>
+        <el-container direction="vertical">
+          <el-main height="">
+            <!-- Main content -->
+          <p>Main content</p>
+          </el-main>
+          <el-footer height="">
+            <!-- Footer content -->
+          </el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+// import { UserModule } from '@/store/modules/user'
+
+@Component({
+  name: 'Top Questions'
+})
+export default class extends Vue {
+  get name() {
+    return 'Top Questions'
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
